cefeb725a53915f55ea5b3065401335d58e5a499
ololadetemi <ololadetoluwalase@gmail.com>
2026-05-11 21:21:28 +0100

    Linked Frontend to the backend

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const pharmacistRoutes = require('./routes/pharmacistroutes');
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'thosc-final.vercel.app', credentials: true }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
