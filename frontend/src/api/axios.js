4adb8da76759870b170a5900c3520886b95b031c
ololadetemi <ololadetoluwalase@gmail.com>
2026-05-11 20:19:30 +0100

    Updated frontend instructions

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '' });
+const api = axios.create({ baseURL: 'https://thosc-hms-backend.onrender.com' });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('thosc_token');
