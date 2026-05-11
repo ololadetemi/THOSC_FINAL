4bbc6b122f8ede517d974ec50b29ab67e593ec0f
ololadetemi <ololadetoluwalase@gmail.com>
2026-05-11 22:30:03 +0100

    Switch to Netlify - add redirects

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const pharmacistRoutes = require('./routes/pharmacistroutes');
 
 const app = express();
 
-app.use(cors({ origin: 'https://thosc-final.vercel.app', credentials: true }));
+app.use(cors({ origin: 'https://thosc-hms.netlify.app', credentials: true }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
